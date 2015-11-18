777ec8bc40fa75ad9315f0029dee82e73164da0e
chloeshiau <chloe.shiau@gmail.com>
2015-11-18 10:58:06 -0800

    all json

--- MammalsDB20150827_el_be.py
+++ MammalsDB20150827_el_be.py
@@ -1,24 +1,37 @@
-import pyodbc
 import json
 import collections
 import MySQLdb
  
 db = MySQLdb.connect(host="localhost", user="root", passwd="ilbaloney.", db="MammalsDB20150827_el_be")
-# conn = pyodbc.connect(connstr)
 cursor = db.cursor()
- 
-cursor.execute("SELECT ID, AnimalID, ArticleID FROM rel_animal_article")
+# cursor.execute("SELECT * FROM rel_animal_article")
+cursor.execute("SHOW TABLES")
+tables = cursor.fetchall()
 
-rows = cursor.fetchall()
-columns = [desc[0] for desc in cursor.description]
-# Convert query to row arrays
+table_nm = []
+for (table_name,) in cursor:
+    # cursor.execute("SELECT * FROM " + table_name)
+    if table_name != 'Paste Errors':
+        table_nm.append(table_name)
 
-rowarray_list = []
-for row in rows:
-    t = dict(zip(columns, row))
-    rowarray_list.append(t)
+print table_nm
 
-j = json.dumps(rowarray_list)
-rowarrays_file = 'student_rowarrays.json'
-f = open(rowarrays_file,'w')
-print >> f, j
+cursor.close()
+
+for name in table_nm:
+	rowarray_list = []
+	cursor = db.cursor()
+	print ("SELECT * FROM " + name)
+	cursor.execute("SELECT * FROM " + name)
+	rows = cursor.fetchall()
+	columns = [desc[0] for desc in cursor.description]
+	
+	for row in rows:
+		t = dict(zip(columns, row))
+		rowarray_list.append(t)
+
+	j = json.dumps(rowarray_list)
+	rowarrays_file = name + '.json'
+	f = open(rowarrays_file,'w')
+	print >> f, j
+	cursor.close()
